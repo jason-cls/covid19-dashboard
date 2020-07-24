4c3858ad353736d4b81e10aea956f5db8ebcd7d9
Jason Chong <jason.chongleeshin@gmail.com>
2020-07-23 21:18:31 -0400

    add dbc to requirements

--- app.py
+++ app.py
@@ -3,87 +3,57 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
-
 import pandas as pd
+import os
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
-
-available_indicators = df['Indicator Name'].unique()
+path_ca = os.path.join(os.getcwd(), 'data', 'covid19canada.csv')
+df_can = pd.read_csv(path_ca)
 
 app.layout = html.Div([
-    html.Div([
+    html.H1('COVID-19 Dashboard '),
 
-        html.Div([
-            dcc.Dropdown(
-                id='xaxis-column',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Fertility rate, total (births per woman)'
-            ),
-            dcc.RadioItems(
-                id='xaxis-type',
-                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                value='Linear',
-                labelStyle={'display': 'inline-block'}
-            )
-        ],
-        style={'width': '48%', 'display': 'inline-block'}),
-
-        html.Div([
-            dcc.Dropdown(
-                id='yaxis-column',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Life expectancy at birth, total (years)'
-            ),
-            dcc.RadioItems(
-                id='yaxis-type',
-                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                value='Linear',
-                labelStyle={'display': 'inline-block'}
-            )
-        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+    dcc.Tabs(id='tabs-granular', value='tab-ca', children=[
+        dcc.Tab(label='Canada', value='tab-ca'),
+        dcc.Tab(label='International', value='tab-int'),
+        dcc.Tab(label='Ontario', value='tab-on')
     ]),
 
-    dcc.Graph(id='indicator-graphic'),
+    html.Div(id='tabs-content')
 
-    dcc.Slider(
-        id='year--slider',
-        min=df['Year'].min(),
-        max=df['Year'].max(),
-        value=df['Year'].max(),
-        marks={str(year): str(year) for year in df['Year'].unique()},
-        step=None
-    )
+
 ])
 
-@app.callback(
-    Output('indicator-graphic', 'figure'),
-    [Input('xaxis-column', 'value'),
-     Input('yaxis-column', 'value'),
-     Input('xaxis-type', 'value'),
-     Input('yaxis-type', 'value'),
-     Input('year--slider', 'value')])
-def update_graph(xaxis_column_name, yaxis_column_name,
-                 xaxis_type, yaxis_type,
-                 year_value):
-    dff = df[df['Year'] == year_value]
 
-    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
-                     y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
-                     hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'])
-
-    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
-
-    fig.update_xaxes(title=xaxis_column_name, 
-                     type='linear' if xaxis_type == 'Linear' else 'log') 
-
-    fig.update_yaxes(title=yaxis_column_name, 
-                     type='linear' if yaxis_type == 'Linear' else 'log') 
-
-    return fig
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs-granular', 'value')])
+def render_tab_content(tab):
+    if tab == 'tab-ca':
+        return html.Div([
+            html.Br(),
+            dcc.Dropdown(
+                id='stat-dropdown',
+                options=[
+                    {'label': 'Total Cases', 'value': 'numtotal'},
+                    {'label': 'Total Deaths', 'value': 'numdeaths'},
+                    {'label': 'Individuals Tested', 'value': 'numtested'},
+                    {'label': 'Cases Recovered', 'value': 'numrecovered'}
+                ],
+                value='numtotal'
+            )
+        ])
+    elif tab == 'tab-int':
+        return html.Div([
+            html.P('PLACEHOLDER TEXT WORLD')
+        ])
+    elif tab == 'tab-on':
+        return html.Div([
+            html.P('PLACEHOLDER TEXT ONTARIO')
+        ])
 
 
 if __name__ == '__main__':
