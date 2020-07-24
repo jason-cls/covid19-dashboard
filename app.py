e10822c79bbe67c98eff5822a8fa29c4c851c118
Jason Chong <jason.chongleeshin@gmail.com>
2020-07-23 22:40:50 -0400

    add dbc layout

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
@@ -7,26 +8,43 @@ import pandas as pd
 import os
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = [dbc.themes.LUX]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 path_ca = os.path.join(os.getcwd(), 'data', 'covid19canada.csv')
 df_can = pd.read_csv(path_ca)
 
-app.layout = html.Div([
-    html.H1('COVID-19 Dashboard '),
-
-    dcc.Tabs(id='tabs-granular', value='tab-ca', children=[
-        dcc.Tab(label='Canada', value='tab-ca'),
-        dcc.Tab(label='International', value='tab-int'),
-        dcc.Tab(label='Ontario', value='tab-on')
-    ]),
-
-    html.Div(id='tabs-content')
-
-
-])
+app.layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.H1('COVID-19 Dashboard'),
+                    width=6
+                ),
+                dbc.Col(
+                    html.Div(
+                        dcc.Tabs(id='tabs-granular', value='tab-ca', children=[
+                            dcc.Tab(label='Canada', value='tab-ca'),
+                            dcc.Tab(label='International', value='tab-int'),
+                            dcc.Tab(label='Ontario', value='tab-on')
+                        ]),
+                    ),
+                    width=6,
+                )
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.Div(id='tabs-content'),
+                    width=3
+                )
+            ]
+        )
+    ]
+)
 
 
 @app.callback(Output('tabs-content', 'children'),
