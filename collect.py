b0dde19ceb653b2c6e86cf6ebf5467c990326e9b
Jason Chong <jason.chongleeshin@gmail.com>
2020-07-30 17:50:41 -0400

    add world data collection

--- collect.py
+++ collect.py
@@ -1,33 +1,10 @@
-#### Canada COVID data processing ####
 import pandas as pd
 import os
 
-
+#### Canada COVID data processing ####
 path_canada = os.path.join(os.getcwd(), 'data', 'covid19canada.csv')
 url = 'https://health-infobase.canada.ca/src/data/covidLive/covid19.csv'
 response = pd.read_csv(url)
-
-# - `pruid`: province id
-# - **`prname`: (English )province name
-# - `prnameFR`: (French) province name
-# - **`date`: date reported
-# - **`numconf`: number of confirmed cases
-# - **`numprob`: number of probable cases
-# - **`numdeaths`: number of deaths
-# - **`numtotal`: total # of confirmed and probable cases
-# - **`numtested`: number of people tested
-# - **`numrecover`: number of people recovered
-# - **`percentrecover`: numrecover / numtotal
-# - `ratetested`:
-# - **`numtoday`: number of new cases relative to yesterday
-# - `percentoday`:  percent change of new cases relative to yesterday
-# - `ratetotal`:
-# - `ratedeaths`:
-# - **`deathstoday`: number of deaths reported today
-# - `percentdeath`:
-# - **`testedtoday`: number of people tested today
-# - `recoveredtoday`: number of people who have recovered today
-# - `percentactive`:
 
 response = response.drop(columns=['pruid', 'prnameFR', 'percentoday',
                                   'ratetested', 'ratetotal', 'ratedeaths',
@@ -46,5 +23,25 @@ for p in provinces:
     for colname in impute_cols:
         response.loc[response['prname'] == p, colname] = response.loc[response['prname'] == p, colname].ffill().fillna(0)
 
-# Save local copy
+# Save data
 response.to_csv(path_canada, index=False)
+
+
+#### World COVID data processing ####
+path_world = os.path.join(os.getcwd(), 'data', 'covid19world.csv')
+url_world = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
+response_world = pd.read_csv(url_world)
+
+# Drop unneeded info
+response_world = response_world.drop(
+    columns=['new_tests_smoothed', 'new_tests_smoothed_per_thousand', 'tests_units',
+             'stringency_index', 'population', 'population_density', 'median_age',
+             'aged_65_older', 'aged_70_older', 'gdp_per_capita', 'extreme_poverty',
+             'cardiovasc_death_rate', 'diabetes_prevalence', 'female_smokers',
+             'male_smokers', 'handwashing_facilities', 'life_expectancy']
+)
+
+response_world['date'] = pd.to_datetime(response_world['date'], yearfirst=True)
+response_world = response_world.sort_values(by=['location', 'date'])
+# Save data
+response_world.to_csv(path_world, index=False)
